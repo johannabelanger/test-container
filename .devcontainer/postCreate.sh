131d3184a9bbe516da64540bc29e565dfe99874c
Johanna Belanger <johanna.belanger@gmail.com>
2024-12-23 06:13:36 +0000

    Prevent failure message if code directories exist

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,7 +1,4 @@
-echo 'Configuring git'
-git config --global user.name "Johanna Belanger"
-git config --global user.email "jbelanger@webtraxllc.com"
-echo 'Configuring postgresql'
+echo "Configuring postgresql"
 service postgresql start
 service postgresql status
 HBA_FILE=$(su -c "psql -t -P format=unaligned -c 'show hba_file'" postgres)
@@ -9,4 +6,16 @@ echo $HBA_FILE
 echo 'host all postgres 127.0.0.1/32 trust' | cat - $HBA_FILE > temp && mv temp $HBA_FILE
 service postgresql stop
 service postgresql status
+echo "Configuring API project"
+if [ ! -d "./test-api" ]; then
+  git clone https://github.com/johannabelanger/test-api.git
+else
+  echo "Project 'test-api' is already configured."
+fi
+echo "Configuring WebApp project"
+if [ ! -d "./test-web-app" ]; then
+  git clone https://github.com/johannabelanger/test-web-app.git
+else
+  echo "Project 'test-web-app' is already configured."
+fi
 echo "Setup complete"
