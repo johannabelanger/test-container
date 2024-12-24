10808ec4ee2e44c2e7c3057abcbe92c2d1920717
Johanna Belanger <johanna.belanger@gmail.com>
2024-12-24 02:06:23 +0000

    run dev servers on start

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -9,15 +9,23 @@ echo 'host all postgres 127.0.0.1/32 trust' | cat - $HBA_FILE > temp && mv temp 
 service postgresql stop
 service postgresql status
 echo "Configuring API project"
+echo "Installing nestjs"
+npm i -g @nestjs/cli
 if [ ! -d "./test-api" ]; then
   git clone https://github.com/johannabelanger/test-api.git
 else
   echo "Project 'test-api' is already configured."
 fi
+cd ./test-api
+npm i
+cd ..
 echo "Configuring WebApp project"
 if [ ! -d "./test-web-app" ]; then
   git clone https://github.com/johannabelanger/test-web-app.git
 else
   echo "Project 'test-web-app' is already configured."
 fi
+cd ./test-web-app
+npm i
+cd ..
 echo "Setup complete"
